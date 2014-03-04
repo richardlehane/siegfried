906aa0c34ad563eb8a8065f7dac8563ba507cba9
Richard Lehane <richard.lehane@gmail.com>
2014-03-04 13:41:01 +1100

    Benchmarks and updated signature suite
    - added benchmarking to the siegfried tool tests (use "go test
    github.com/richardlehane/siegfried/cmd/siegfried -bench=.")
    - updated Ross's signature suite. All these tests should now pass.

--- cmd/siegfried/siegfried_test.go
+++ cmd/siegfried/siegfried_test.go
@@ -7,15 +7,22 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/richardlehane/siegfried/pkg/core/bytematcher"
 	"github.com/richardlehane/siegfried/pkg/pronom"
 )
 
 var testSigs = filepath.Join("..", "r2d2", "data", "pronom.gob")
 
+var b *bytematcher.Bytematcher
+
 func init() {
 	var err error
 	droid, _, reports := pronom.ConfigPaths()
 	puids, err = pronom.PuidsFromDroid(droid, reports)
+	if err != nil {
+		panic(err)
+	}
+	b, err = load(testSigs)
 	if err != nil {
 		panic(err)
 	}
@@ -76,10 +83,6 @@ func TestSuite(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	b, err := load(testSigs)
-	if err != nil {
-		t.Fatal(err)
-	}
 	matches, err := multiIdentify(b, root)
 	if err != nil {
 		t.Fatal(err)
@@ -98,6 +101,102 @@ func TestSuite(t *testing.T) {
 	}
 	if iter != len(expect) {
 		t.Errorf("Matched %v out of %v signatures", iter, len(expect))
-		t.Error(b.Stats())
+	}
+}
+
+// Benchmarks
+func BenchmarkNew(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		load(testSigs)
+	}
+}
+
+func benchidentify(ext string) {
+	file := filepath.Join(".", "testdata", "benchmark", "Benchmark")
+	file += "." + ext
+	identify(b, file)
+}
+
+func BenchmarkACCDB(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("accdb")
+	}
+}
+
+func BenchmarkBMP(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("bmp")
+	}
+}
+
+func BenchmarkDOCX(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("docx")
+	}
+}
+
+func BenchmarkGIF(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("gif")
+	}
+}
+
+func BenchmarkJPG(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("jpg")
+	}
+}
+
+func BenchmarkMSG(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("msg")
+	}
+}
+
+func BenchmarkODT(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("odt")
+	}
+}
+
+func BenchmarkPDF(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("pdf")
+	}
+}
+
+func BenchmarkPNG(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("png")
+	}
+}
+
+func BenchmarkPPTX(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("pptx")
+	}
+}
+
+func BenchmarkRTF(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("rtf")
+	}
+}
+
+func BenchmarkTIF(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("tif")
+	}
+}
+
+func BenchmarkXLSX(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("xlsx")
+	}
+}
+
+func BenchmarkXML(bench *testing.B) {
+	for i := 0; i < bench.N; i++ {
+		benchidentify("xml")
 	}
 }
