d6cd6131968438298c644e17bcf3092b1006924b
Richard Lehane <richard.lehane@gmail.com>
2015-04-22 17:42:59 +1000

    set theory

--- cmd/roy/sets.go
+++ cmd/roy/sets.go
@@ -15,10 +15,16 @@
 package main
 
 import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 
 	"gopkg.in/yaml.v2"
+
+	"github.com/richardlehane/siegfried/config"
 )
 
 func replaceKeys(l string) string {
@@ -27,6 +33,11 @@ func replaceKeys(l string) string {
 	for _, v := range items {
 		item := strings.TrimSpace(v)
 		if strings.HasPrefix(item, "@") {
+			if sets == nil {
+				if err := initSets(); err != nil {
+					panic(err)
+				}
+			}
 			list, err := getSets(strings.TrimPrefix(item, "@"))
 			if err != nil {
 				panic(err)
@@ -49,20 +60,71 @@ func replaceKeys(l string) string {
 var sets map[string][]string
 
 func getSets(key string) ([]string, error) {
-	if sets == nil {
-		if err := initSets(); err != nil {
-			return nil, err
+	// recursively build a list of all values for the key; prevent cycles by bookkeeping with attempted map
+	attempted := make(map[string]bool)
+	var f func(string) ([]string, error)
+	f = func(k string) ([]string, error) {
+		if ok := attempted[k]; ok {
+			return nil, nil
 		}
+		attempted[k] = true
+		l, ok := sets[k]
+		if !ok {
+			return nil, errors.New("sets: unknown key " + k)
+		}
+		for _, k2 := range l {
+			if strings.HasPrefix(k2, "@") {
+				l2, err := f(strings.TrimPrefix(k2, "@"))
+				if err != nil || l2 == nil {
+					return nil, err
+				}
+				l = append(l, l2...)
+			}
+		}
+		return l, nil
 	}
-	return sets["a"], nil
-
-	// 2. expand any keys into lists of fmts
-	// 3. recursively expand keys within those lists. Prevent cycles by adding keys already expanded to a stop list
-	// 4. get a comma separted list as input, return a comma separated list as output
+	return f(key)
 }
 
 func initSets() error {
-	// 1. load all files in the sets directory and, if no yaml error, add them to a single map
+	//  load all yaml files in the sets directory and add them to a single map
 	sets = make(map[string][]string)
-	return yaml.Unmarshal([]byte{1, 2}, &sets)
+	wf := func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		switch filepath.Ext(path) {
+		default:
+			return nil
+		case "yml", "yaml":
+		}
+		set := make(map[string][]string)
+		byts, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		err = yaml.Unmarshal(byts, &set)
+		if err != nil {
+			return err
+		}
+		for k, v := range set {
+			sort.Strings(v)
+			m, ok := sets[k]
+			if !ok {
+				sets[k] = v
+			} else {
+				// if we already have this key, add any new items in its list to the existing list
+				for _, w := range v {
+					idx := sort.SearchStrings(m, w)
+					if idx == len(m) || m[idx] != w {
+						m = append(m, w)
+					}
+				}
+				sort.Strings(m)
+				sets[k] = m
+			}
+		}
+		return nil
+	}
+	return filepath.Walk(filepath.Join(config.Home(), "sets"), wf)
 }
