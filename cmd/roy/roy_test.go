8adfd8d777db8b1c8a9b7e3fa7950a3e6bbb55a1
Richard Lehane <richard.lehane@gmail.com>
2016-03-12 20:54:53 +1100

    new pronom-tika.sig

--- cmd/roy/roy_test.go
+++ cmd/roy/roy_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/richardlehane/siegfried"
 	"github.com/richardlehane/siegfried/config"
+	"github.com/richardlehane/siegfried/pkg/mimeinfo"
 	"github.com/richardlehane/siegfried/pkg/pronom"
 )
 
@@ -24,6 +25,32 @@ func TestMakeGob(t *testing.T) {
 		t.Fatal(err)
 	}
 	sigs := filepath.Join("data", config.SignatureBase())
+	err = s.Save(sigs)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMakePronomTika(t *testing.T) {
+	s := siegfried.New()
+	config.SetHome(*testhome)
+	p, err := pronom.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Add(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	m, err := mimeinfo.New(config.SetName("tika"), config.SetMIMEInfo("tika"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Add(m)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sigs := filepath.Join("data", "pronom-tika.sig")
 	err = s.Save(sigs)
 	if err != nil {
 		t.Fatal(err)
