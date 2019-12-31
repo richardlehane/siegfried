6c6decbb9ff66aed39727b49f843a12e44cc5256
Richard Lehane <richard.lehane@gmail.com>
2019-12-31 10:50:41 +0100

    fix tests

--- internal/bytematcher/patterns/bmh_test.go
+++ internal/bytematcher/patterns/bmh_test.go
@@ -14,14 +14,11 @@ func TestBMH(t *testing.T) {
 		t.Error("BMH equality fail")
 	}
 	ok, l := b.Test([]byte("test"))
-	if ok < 0 {
-		t.Error("Expecting bmh to match test")
-	}
-	if ok != 4 {
+	if len(ok) != 1 || ok[0] != 4 {
 		t.Errorf("Expecting bmh match length to be 4, got %d", l)
 	}
 	ok, l = b.Test([]byte("tost"))
-	if ok > -1 {
+	if len(ok) > 0 {
 		t.Error("Not expecting bmh to match tost")
 	}
 	if l != 3 {
@@ -32,14 +29,11 @@ func TestBMH(t *testing.T) {
 func TestRBMH(t *testing.T) {
 	b := NewRBMHSequence(TestSequences[0])
 	ok, l := b.TestR([]byte("tosttest"))
-	if ok < 0 {
-		t.Error("Expecting bmh to match test")
-	}
-	if ok != 4 {
+	len(ok) != 1 || ok[0] != 4 {
 		t.Errorf("Expecting bmh match length to be 4, got %d", l)
 	}
 	ok, l = b.TestR([]byte("testtost"))
-	if ok > -1 {
+	if len(ok) > 0 {
 		t.Error("Not expecting bmh to match tost")
 	}
 	if l != 3 {
