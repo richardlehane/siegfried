6c1f37974d06b2a4727b8a82b1293fb2bf75517f
Richard Lehane <richard@itforarchivists.com>
2023-05-27 12:00:45 +0200

    implements globs for container matching see https://github.com/digital-preservation/pronom/issues/10

--- pkg/config/siegfried.go
+++ pkg/config/siegfried.go
@@ -47,7 +47,7 @@ var siegfried = struct {
 	checkpoint int64
 	userAgent  string
 }{
-	version:         [3]int{1, 10, 1},
+	version:         [3]int{1, 11, 0},
 	signature:       "default.sig",
 	conf:            "sf.conf",
 	magic:           []byte{'s', 'f', 0x00, 0xFF},
