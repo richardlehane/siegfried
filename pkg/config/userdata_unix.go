ec0109f0b2cafc2500e470f66ce4a9b4289a31a6
Richard Lehane <richard@itforarchivists.com>
2024-06-02 15:34:01 +1000

    build tags

--- pkg/config/userdata_unix.go
+++ pkg/config/userdata_unix.go
@@ -1,4 +1,4 @@
-//go:build (aix || dragonfly || freebsd || nacl || linux || netbsd || openbsd || solaris) && !brew && !archivematica && !js
+//go:build aix || dragonfly || freebsd || nacl || linux || netbsd || openbsd || solaris
 
 // Copyright 2014 Richard Lehane. All rights reserved.
 //
