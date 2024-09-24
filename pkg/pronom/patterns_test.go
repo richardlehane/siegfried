7cd90a45c0886a0e0d9c7bc5d6dd69fc242049d8
Richard Lehane <richard@itforarchivists.com>
2024-09-24 20:19:59 +1000

    PRONOM v119

--- pkg/pronom/patterns_test.go
+++ pkg/pronom/patterns_test.go
@@ -10,6 +10,7 @@ func TestRange(t *testing.T) {
 	rng := Range{[]byte{1}, []byte{3}}
 	rng2 := Range{[]byte{1}, []byte{3}}
 	rng3 := Range{[]byte{11, 250}, []byte{12, 1}}
+	rng4 := Range{[]byte{00, 00}, []byte{10, 00}}
 	if !rng.Equals(rng2) {
 		t.Error("Range fail: Equality")
 	}
@@ -19,13 +20,16 @@ func TestRange(t *testing.T) {
 	if r, _ := rng.Test([]byte{2}); len(r) != 1 || r[0] != 1 {
 		t.Error("Range fail: Test")
 	}
-	if r, _ := rng.Test([]byte{3}); len(r) != 1 || r[0] != 1 {
+	if r, _ := rng.Test([]byte{3}); len(r) != 1 && r[0] != 1 {
 		t.Error("Range fail: Test")
 	}
 	if r, _ := rng.Test([]byte{4}); len(r) > 0 {
 		t.Error("Range fail: Test should fail")
 	}
-	if r, _ := rng3.Test([]byte{11, 251}); len(r) != 1 || r[0] != 2 {
+	if r, _ := rng3.Test([]byte{11, 251}); len(r) != 1 && r[0] != 2 {
+		t.Errorf("Range fail: Test multibyte range, got %d, %d", len(r), r[0])
+	}
+	if r, _ := rng4.Test([]byte{10, 00}); len(r) != 1 && r[0] != 2 {
 		t.Errorf("Range fail: Test multibyte range, got %d, %d", len(r), r[0])
 	}
 	if rng.NumSequences() != 3 {
